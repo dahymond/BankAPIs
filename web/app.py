826aad93cb814b8e8a712d5a6ee41fa46f3f00c9
Solomon Sunday <ssdahymond@gmail.com>
2022-05-12 23:13:40 -0500

    init bankapis project

---
+++ web/app.py
@@ -0,0 +1,66 @@
+from flash import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+
+
+app = Flask(__name__)
+
+api = Api(app)
+client = MongoClient("mongodb://db:27017")
+db = client.BankAPI
+users = db["Users"]
+
+
+def UserExist(username):
+    if users.find({Username: username}).count()==0:
+        return False
+    else:
+        return True
+    
+class Register(Resource):
+    def post(self):
+        postedData = request.get_json()
+        
+        username = postedData["username"]
+        password = postedData["password"]
+        
+        if UserExist(username):
+            retJson = {
+                "status": "301",
+                "msg": "Invalid Username"
+            }
+            return jsonify(retJson)
+        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        
+        users.insert({
+            "Username": username,
+            "Passowrd": hash_pw,
+            "Own": 0,
+            "Debt": 0
+        })
+        
+        retJson = {
+            "status": 200,
+            "msg": "You successfully signed up for the API"
+        }
+        return jsonify(retJson)
+    
+    def verifyPw(username, password):
+        if not UserExist(username):
+            return False
+        
+        hashed_pw = users.find({
+            "Username": username
+        })[0]["Password"]
+        
+        if bcrypt.hashpw(password.encode('utf8'), hashed_pw)==hashed_pw:
+            return True
+        else:
+            return False
+    
+    def cashWithUser(username):
+        cash = users.find({
+            "Username":username
+        })[0]["Own"]
+        return cash
